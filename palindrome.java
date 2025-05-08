c58f771bedc5dec20a6d672556a7697b331e6bf2
grvijayk <G-R-IMMANUEL.VIJAYKUMAR@CAPGEMINI.COM>
2025-05-08 05:33:12 +0000

    Added palindrome.java
    git add palindrome.java
    git commit -m "Add palindrome.java"
    
    git push origin main

---
+++ palindrome.java
@@ -0,0 +1,19 @@
+//write simple code for palindrome
+import java.util.Scanner;
+
+public class palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String str = sc.nextLine();
+        String rev = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev += str.charAt(i);
+        }
+        if (str.equals(rev)) {
+            System.out.println("The string is a palindrome.");
+        } else {
+            System.out.println("The string is not a palindrome.");
+        }sc.close();
+    }
+}
